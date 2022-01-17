757c6ee56528bf6e4ac31948cc10def85fae1be3
たくまっち   Takumachi <47268002+xtakumatutix@users.noreply.github.com>
2022-01-18 01:27:48 +0900

    ファイル名変更、初期メッセージ変更

---
+++ src/xtakumatutix/jsm/Main.php
@@ -0,0 +1,26 @@
+<?php
+namespace xtakumatutix\jsm;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\Player;
+use pocketmine\utils\Config;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+
+class Main extends PluginBase implements Listener {
+
+    /** @var string */
+    private $message;
+
+    public function onEnable() :void{
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $config = new Config($this->getDataFolder() . "config.yml", Config::YAML, [
+            '送信したいメッセージ' => 'これはプラグインの初期メッセージです{br}config.ymlを変更してください'
+        ]);
+        $this->message = str_replace("{br}", "\n", $config->get("送信したいメッセージ"));
+    }
+
+    public function onJoin(PlayerJoinEvent $event){
+        $event->getPlayer()->sendMessage($this->message);
+    }
+}
